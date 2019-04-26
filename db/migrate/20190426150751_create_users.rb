afbdc089ee46441854ed24ad1125c23291206882
ScottSlatton <scottslatton.tech@gmail.com>
2019-04-26 08:51:09 -0700

    generated initial migrations

--- db/migrate/20190426150751_create_users.rb
+++ db/migrate/20190426150751_create_users.rb
@@ -1,6 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      t.string :username
+      t.integer :level
+      t.integer :experience
+      t.string :friend_code
 
       t.timestamps
     end
--- db/migrate/20190426150816_create_crafts.rb
+++ db/migrate/20190426150816_create_crafts.rb
@@ -1,6 +1,9 @@
 class CreateCrafts < ActiveRecord::Migration[5.2]
   def change
     create_table :crafts do |t|
+      t.string :name
+      t.integer :req_score
+      t.integer :damage
 
       t.timestamps
     end
